49eb78103134aabc132a5ead304fdec2b478b3ee
Saifulnovel <sinovel07@gmail.com>
2022-11-09 20:21:27 +0600

    dataload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 
@@ -10,6 +11,30 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6h4mfox.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run(){
+    try{
+        const photoCollection = client.db('goWild').collection('photos')
+        app.get('/photos', async (req, res) => {
+            const query = {};
+            const cursor = photoCollection.find(query);
+            const photos = await cursor.toArray();
+            res.send(photos)
+        })
+    }
+    finally{
+        
+    }
+}
+ run().catch(err => console.error(err))
 
 app.get('/', (req, res) => {
     res.send('get wild server is running')
